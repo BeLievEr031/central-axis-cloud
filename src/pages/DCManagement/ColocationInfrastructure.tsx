7db1c25c7827277f24c8d9a1dcf265aeaba61b5b
BeLieVer031 <sandyrajak031@gmail.com>
2025-05-20 17:50:54 +0530

    fix: removed unused variable

--- src/pages/DCManagement/ColocationInfrastructure.tsx
+++ src/pages/DCManagement/ColocationInfrastructure.tsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 const ColocationInfrastructure = () => {
     return (
         <article className="max-w-4xl mx-auto px-4 py-8">
--- src/pages/DCManagement/IPAM.tsx
+++ src/pages/DCManagement/IPAM.tsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 const ipamFeatures = [
     {
         title: 'Automated Discovery',
